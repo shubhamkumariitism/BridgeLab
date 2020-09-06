ade0720ba366128ea43124ca986e3da714645882
Shubham Kumar <kumarshub18@gmail.com>
2020-09-06 20:31:47 +0530

    Employee Wage UC4 Code

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,31 @@
-#!/bin/bash -x
-echo"Welocme to Employee Wage Computaion Program on Master Branch"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#UC4
+case PresentOrAbsent in
+                1)
+                DailyWageEmployeeFullDay=$(( $WageperHour * $FullDayHour ))
+				echo DailyWageEmployeeFullDay
+                ;;
+
+                2)
+                DailyWageEmployeePartDay=$(( $WageperHour * $PartTimeHour ))
+				echo DailyWageEmployeePartDay
+                ;;
+                *)
+                empHrs=0
+                ;;
+esac

0877727a6bd2ff8b42e260bb93d25d0e19be628e
Shubham Kumar <kumarshub18@gmail.com>
2020-09-06 20:35:46 +0530

    Employee Wage UC7 Code

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,71 @@
-#!/bin/bash -x
-echo"Welocme to Employee Wage Computaion Program on Master Branch"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#UC7
+getWorkingDays()
+{
+         case $1 in
+                1)
+                empHrs=$FullDayHour
+                ;;
+
+                2)
+                empHrs=$PartTimeHour
+                ;;
+                *)
+                empHrs=0
+                ;;
+        esac
+        return $empHrs
+
+}
+#END OF UC7

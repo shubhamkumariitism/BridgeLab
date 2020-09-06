f780afb6b7fa95a9915f56074d8845fef7c905c4
Shubham Kumar <kumarshub18@gmail.com>
2020-09-06 20:34:24 +0530

    Employee Wage UC6 Code

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,87 @@
-#!/bin/bash -x
-echo"Welocme to Employee Wage Computaion Program on Master Branch"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#UC6
+totalEmpHrs=0
+totalWorkingDays=0
+MAX_HRS=100
+MAX_WORKING_DAYS=20
+while [[ $totalEmpHrs -lt $MAX_HRS && $totalWorkingDays -lt $MAX_WORKING_DAYS  ]]
+do
+        ((totalWorkingDays++))
+		randomCheck=$((RANDOM%3))
+        getWorkingDays $randomCheck
+		empHrs=$?
+        dailyWage=$(( $empHrs * $WageperHour ))
+        dailyWageArray["Day"$totalWorkingDays]=$dailyWage
+        totalEmpHrs=$(($totalEmpHrs+$empHrs))
+done

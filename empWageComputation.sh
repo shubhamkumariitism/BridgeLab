6e44422cf748fbe276e363e1f93cb0b368d5e44f
Shubham Kumar <kumarshub18@gmail.com>
2020-09-06 20:33:20 +0530

    Employee Wage UC5 Code

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,51 @@
-#!/bin/bash -x
-echo"Welocme to Employee Wage Computaion Program on Master Branch"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#UC5
+
+EmpWorkingPerMonth=20
+case PresentOrAbsent in
+                1)
+                DailyWageEmployeeFullDay=$(( $WageperHour * $FullDayHour ))
+				EmpWageForMonth=$(( $DailyWageEmployeeFullDay *$EmpWorkingPerMonth ))
+				echo "Employee Wage For a Month For Full Time" $EmpWageForMonth
+                ;;
+
+                2)
+                DailyWageEmployeePartDay=$(( $WageperHour * $PartTimeHour ))
+				EmpWageForMonth=$(( $DailyWageEmployeePartDay *$EmpWorkingPerMonth ))
+				echo "Employee Wage For a Month For Part Time" $EmpWageForMonth
+                ;;
+                *)
+                empHrs=0
+                ;;
+esac
